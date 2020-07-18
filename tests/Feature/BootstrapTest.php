a5c1de22c7d505e917c6c49b97e285282d8f8b7c
pascalbaljet <pascal@pascalbaljet.nl>
2020-07-18 10:30:26 +0200

    Added bootstrap specific tests

---
+++ tests/Feature/BootstrapTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace ProtoneMedia\LaravelFormComponents\Tests\Feature;
+
+use ProtoneMedia\LaravelFormComponents\Tests\TestCase;
+
+class TranslationTest extends TestCase
+{
+    /** @test */
+    public function it_can_append_to_an_input()
+    {
+        $this->registerTestRoute('bootstrap-append');
+
+        config(['form-components.framework' => 'bootstrap-4']);
+
+        $this->visit('/bootstrap-append')
+            ->seeInElement('.input-group-text', '.protone.media');
+    }
+
+    /** @test */
+    public function it_can_prepend_to_an_input()
+    {
+        $this->registerTestRoute('bootstrap-prepend');
+
+        config(['form-components.framework' => 'bootstrap-4']);
+
+        $this->visit('/bootstrap-prepend')
+            ->seeInElement('.input-group-text', 'info@');
+    }
+
+    /** @test */
+    public function it_can_add_help_text()
+    {
+        $this->registerTestRoute('bootstrap-help');
+
+        config(['form-components.framework' => 'bootstrap-4']);
+
+        $this->visit('/bootstrap-help')
+            ->seeInElement('.form-text', 'Your username must be 8-20 characters long.');
+    }
+}
---
+++ tests/Feature/views/bootstrap-prepend.blade.php
@@ -0,0 +1,7 @@
+<x-form>
+    <x-form-input name="input">
+        @slot('prepend')
+            info@
+        @endslot
+    </x-form-input>
+</x-form>
