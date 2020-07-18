30435a5f40142d1536dc01b98b2e6c954e568c42
pascalbaljet <pascal@pascalbaljet.nl>
2020-07-18 09:24:01 +0200

    Better error handling

--- src/Components/FormSubmit.php
+++ src/Components/FormSubmit.php
@@ -4,15 +4,4 @@ namespace ProtoneMedia\LaravelFormComponents\Components;
 
 class FormSubmit extends Component
 {
-    public string $label;
-
-    /**
-     * Create a new component instance.
-     *
-     * @return void
-     */
-    public function __construct(string $label = '')
-    {
-        $this->label = $label ?: __('Submit');
-    }
 }
--- src/Components/HandlesValidationErrors.php
+++ src/Components/HandlesValidationErrors.php
@@ -2,7 +2,40 @@
 
 namespace ProtoneMedia\LaravelFormComponents\Components;
 
+use Illuminate\Support\ViewErrorBag;
+
 trait HandlesValidationErrors
 {
     public $showErrors = true;
+
+    /**
+     * Returns a boolean wether the given attribute has an error
+     * and the should be shown.
+     *
+     * @param string $name
+     * @param string $bag
+     * @return boolean
+     */
+    public function hasErrorAndShow(string $name, string $bag = 'default'): bool
+    {
+        return $this->showErrors
+            ? $this->hasError($name, $bag)
+            : false;
+    }
+
+    /**
+     * Returns a boolean wether the given attribute has an error.
+     *
+     * @param string $name
+     * @param string $bag
+     * @return boolean
+     */
+    public function hasError(string $name, string $bag = 'default'): bool
+    {
+        $errors = request()->session()->get('errors') ?: new ViewErrorBag;
+
+        $name = str_replace(['[', ']'], ['.', ''], $name);
+
+        return $errors->getBag($bag)->has($name);
+    }
 }
