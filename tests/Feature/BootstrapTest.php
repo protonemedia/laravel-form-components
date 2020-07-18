2c21e13c5f3f015c47c4e01ee9a5b88be974bed0
pascalbaljet <pascal@pascalbaljet.nl>
2020-07-18 10:37:54 +0200

    Update BootstrapTest.php

--- tests/Feature/BootstrapTest.php
+++ tests/Feature/BootstrapTest.php
@@ -6,12 +6,19 @@ use ProtoneMedia\LaravelFormComponents\Tests\TestCase;
 
 class BootstrapTest extends TestCase
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        if (config('form-components.framework') !== 'bootstrap-4') {
+            $this->markTestSkipped('Other framework configured');
+        }
+    }
+
     /** @test */
     public function it_can_append_to_an_input()
     {
         $this->registerTestRoute('bootstrap-append');
-
-        config(['form-components.framework' => 'bootstrap-4']);
 
         $this->visit('/bootstrap-append')
             ->seeInElement('.input-group-text', '.protone.media');
@@ -22,8 +29,6 @@ class BootstrapTest extends TestCase
     {
         $this->registerTestRoute('bootstrap-prepend');
 
-        config(['form-components.framework' => 'bootstrap-4']);
-
         $this->visit('/bootstrap-prepend')
             ->seeInElement('.input-group-text', 'info@');
     }
@@ -32,8 +37,6 @@ class BootstrapTest extends TestCase
     public function it_can_add_help_text()
     {
         $this->registerTestRoute('bootstrap-help');
-
-        config(['form-components.framework' => 'bootstrap-4']);
 
         $this->visit('/bootstrap-help')
             ->seeInElement('.form-text', 'Your username must be 8-20 characters long.');
