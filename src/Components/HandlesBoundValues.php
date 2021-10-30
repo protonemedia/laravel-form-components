84611aaf7577e76c4b2cc1014ac18ade946933da
pascalbaljet <pascal@pascalbaljet.nl>
2021-10-30 16:14:53 +0200

    Support for Date and DateTime casting, fixes #53

--- src/Components/HandlesBoundValues.php
+++ src/Components/HandlesBoundValues.php
@@ -2,9 +2,11 @@
 
 namespace ProtoneMedia\LaravelFormComponents\Components;
 
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Support\Carbon;
 use ProtoneMedia\LaravelFormComponents\FormDataBinder;
 
 trait HandlesBoundValues
@@ -52,9 +54,51 @@ trait HandlesBoundValues
 
         $bind = $bind ?: $this->getBoundTarget();
 
-        return $this->manyRelation
-            ? $this->getAttachedKeysFromRelation($bind, $name)
-            : data_get($bind, $name);
+        if ($this->manyRelation) {
+            return  $this->getAttachedKeysFromRelation($bind, $name);
+        }
+
+        $boundValue = data_get($bind, $name);
+
+        if ($bind instanceof Model && $boundValue instanceof DateTimeInterface) {
+            return $this->formatDateTime($bind, $name, $boundValue);
+        }
+
+        return $boundValue;
+    }
+
+    /**
+     * Formats a DateTimeInterface if the key is specified as a date or datetime in the model.
+     *
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @param string $key
+     * @param DateTimeInterface $date
+     * @return void
+     */
+    private function formatDateTime(Model $model, string $key, DateTimeInterface $date)
+    {
+        $cast = $model->getCasts()[$key] ?? null;
+
+        if (!$cast || $cast === 'date' || $cast === 'datetime') {
+            return Carbon::instance($date)->toJSON();
+        }
+
+        if ($this->isCustomDateTimeCast($cast)) {
+            return $date->format(explode(':', $cast, 2)[1]);
+        }
+
+        return $date;
+    }
+
+    /**
+     * Determine if the cast type is a custom date time cast.
+     *
+     * @param  string  $cast
+     * @return bool
+     */
+    protected function isCustomDateTimeCast($cast)
+    {
+        return strncmp($cast, 'date:', 5) === 0 || strncmp($cast, 'datetime:', 9) === 0;
     }
 
     /**
---
+++ tests/Feature/views/date-time-casts.blade.php
@@ -0,0 +1,11 @@
+<x-form>
+    @bind($model)
+        <x-form-input name="date_a" />
+        <x-form-input name="date_b" />
+        <x-form-input name="date_c" />
+        <x-form-input name="date_d" />
+        <x-form-input name="date_e" />
+
+        <x-form-submit />
+    @endbind
+</x-form>
